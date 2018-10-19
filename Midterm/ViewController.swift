bfe4b507ef85122a04c3f72430e92d8f21624b1a
thayllananacleto <thayllananacleto@gmail.com>
2018-10-18 20:30:46 -0400

    Slight changes to the design and constraints.

--- Midterm/ViewController.swift
+++ Midterm/ViewController.swift
@@ -1,20 +1,39 @@
 //
-//  ViewController.swift
 //  Midterm
 //
-//  Created by Thayllan Anacleto on 2018-10-18.
+//  File name: ViewController.swift
+//  Author: Thayllan Anacleto
+//  Student ID: 300973606
+//  Date: 2018-10-18
+//
 //  Copyright © 2018 Thayllan Anacleto. All rights reserved.
 //
 
 import UIKit
 
+// ViewController class for the midterm app
 class ViewController: UIViewController {
+    
+    // IBOutlets
+    @IBOutlet weak var lblItem01: UILabel!
+    @IBOutlet weak var lblItem02: UILabel!
+    @IBOutlet weak var lblItem03: UILabel!
+    @IBOutlet weak var lblItem04: UILabel!
+    @IBOutlet weak var lblItem05: UILabel!
+    @IBOutlet weak var lblItem06: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // Method called when there's a change in quantity for any of the items
+    @IBAction func itemChanged(_ sender: UIStepper) {
+        
+        
+        
+    }
+    
 
 }
 
